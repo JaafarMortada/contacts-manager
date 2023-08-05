8de0060c464f54da17865b1f2287f9a800200bc0
Jaafar Mortada <jaafar1899@outlook.com>
2023-08-05 13:53:38 +0300

    added signup page

---
+++ frontend/src/signin-signup/signup.jsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+import logo from "../CONTACT MANAGER.svg";
+import "./styles.css";
+
+const Signup = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [verpassword, setVerpassword] = useState("");
+
+  return (
+    <div className="container">
+      <div className="form form-signup">
+        <div class="logo">
+          <img src={logo} className="App-logo" alt="logo" />
+          <span className="title">Signup</span>
+        </div>
+        <div class="input-with-label">
+          <label>Enter your name:</label>
+          <input
+            type="text"
+            placeholder="Please enter your name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div class="input-with-label">
+          <label>Enter your email:</label>
+          <input
+            type="email"
+            placeholder="Enter your email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div class="input-with-label">
+          <label>Enter your password:</label>
+          <input
+            type="password"
+            placeholder="Enter your password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div class="input-with-label">
+          <label>Verify your password:</label>
+          <input
+            type="password"
+            placeholder="Verify your password"
+            value={verpassword}
+            onChange={(e) => setVerpassword(e.target.value)}
+          />
+        </div>
+        <div class="btn-link">
+          <button type="button" class="signup-btn" id="signup-btn">
+            Signup
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
