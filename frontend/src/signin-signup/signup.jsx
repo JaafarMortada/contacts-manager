bbe78aeb7349c97e65728a702ee5ab77813a5c78
Jaafar Mortada <jaafar1899@outlook.com>
2023-08-05 22:22:02 +0300

    added signup fetch

--- frontend/src/signin-signup/signup.jsx
+++ frontend/src/signin-signup/signup.jsx
@@ -1,11 +1,35 @@
 import { useState } from "react";
 import logo from "../CONTACT MANAGER.svg";
+import axios from "axios";
+import { useNavigate } from "react-router-dom"
 
 const Signup = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+    password: ""
+  })
   const [verpassword, setVerpassword] = useState("");
+
+  const handleDataChange = (e)=>{
+    setData({...data, [e.target.name]: e.target.value})
+  }
+
+  const handleSubmit = async ()=>{
+    if(data.name && data.email && data.password && data.password===verpassword){
+      try{
+      const response = await axios.post("http://127.0.0.1:8000/api/register", data)
+      const status = response.data.status
+      if (status==='Success'){
+        navigate("/")
+      }
+    }catch(e){
+      console.log(e)
+    }
+    }
+
+  }
 
   return (
     <div className="container">
@@ -17,28 +41,31 @@ const Signup = () => {
         <div class="input-with-label">
           <label>Enter your name:</label>
           <input
+            name="name"
             type="text"
             placeholder="Please enter your name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
+            value={data.name}
+            onChange={handleDataChange}
           />
         </div>
         <div class="input-with-label">
           <label>Enter your email:</label>
           <input
+            name="email"
             type="email"
             placeholder="Enter your email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            value={data.email}
+            onChange={handleDataChange}
           />
         </div>
         <div class="input-with-label">
           <label>Enter your password:</label>
           <input
+            name="password"
             type="password"
             placeholder="Enter your password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            value={data.password}
+            onChange={handleDataChange}
           />
         </div>
         <div class="input-with-label">
@@ -51,7 +78,12 @@ const Signup = () => {
           />
         </div>
         <div class="btn-link">
-          <button type="button" class="signup-btn" id="signup-btn">
+          <button 
+          type="button" 
+          class="signup-btn" 
+          id="signup-btn"
+          onClick={handleSubmit}
+          >
             Signup
           </button>
         </div>
