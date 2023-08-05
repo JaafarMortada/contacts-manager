fecf81483cb8428c5df8d87ccda6ff2b35178ee2
Jaafar Mortada <jaafar1899@outlook.com>
2023-08-05 22:41:54 +0300

    added signin fetch

--- frontend/src/signin-signup/signin.jsx
+++ frontend/src/signin-signup/signin.jsx
@@ -1,11 +1,37 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../CONTACT MANAGER.svg';
-
+import { useNavigate } from "react-router-dom"
+import axios from "axios";
 const Signin = () => {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+    localStorage.clear()
+    const navigate = useNavigate();
+    const [data, setData] = useState({
+      email: "",
+      password: ""
+    })
 
+    const handleDataChange = (e)=>{
+      setData({...data, [e.target.name]: e.target.value})
+    }
+  
+    const handleSubmit = async ()=>{
+      if(data.email && data.password){
+        try{
+        const response = await axios.post("http://127.0.0.1:8000/api/login", data)
+        const status = response.data.status
+        if (status==='Success'){
+          localStorage.setItem("token", response.data.data.token)
+          navigate("/home")
+        } 
+      }catch(e){
+        const signin_btn = document.getElementById("signin-btn")
+        signin_btn.innerHTML = 'Failed'
+        setTimeout(() => { signin_btn.innerHTML = 'Signin' }, 3000)
+      }
+      }
+  
+    }
     return (
             <div className='container'>
               <div className='form'>
@@ -16,25 +42,32 @@ const Signin = () => {
               <div class="input-with-label">
                   <label>Enter your email:</label>
                   <input 
+                  name="email"
                   type="email" 
                   placeholder="Enter your email" 
-                  value={ email }
-                  onChange={ e => setEmail(e.target.value)}
+                  value={ data.email }
+                  onChange={handleDataChange}
                   />
               </div>
               <div class="input-with-label">
                   <label>Enter your password:</label>
                   <input 
+                  name="password"
                   type="password" 
                   placeholder="Enter your password"
-                  value={ password }
-                  onChange={ e => setPassword(e.target.value)}
+                  value={ data.password }
+                  onChange={handleDataChange}
                   />
               </div>
               
               <div class="btn-link">
                   <Link to='/signup' className='go-sign-up'>Don't have an account?<br/>Create one here!</Link>
-                  <Link to='/home' type="button" className="signin-btn" id="signin-btn">Signin</Link>
+                  <button 
+                  type="button" 
+                  className="signin-btn" 
+                  id="signin-btn"
+                  onClick={handleSubmit}
+                  >Signin</button>
               </div>
               </div>
             </div>        
