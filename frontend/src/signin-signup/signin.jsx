61b98f0b052b5e7e24c2bb442c41703b400228a7
Jaafar Mortada <jaafar1899@outlook.com>
2023-08-05 13:53:24 +0300

    added signin page

---
+++ frontend/src/signin-signup/signin.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../CONTACT MANAGER.svg';
+import './styles.css'
+
+const Signin = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    return (
+            <div className='container'>
+              <div className='form'>
+              <div class="logo">
+                <img src={logo} className="App-logo" alt="logo" />
+                <span className="title">Signin</span>
+              </div>
+              <div class="input-with-label">
+                  <label>Enter your email:</label>
+                  <input 
+                  type="email" 
+                  placeholder="Enter your email" 
+                  value={ email }
+                  onChange={ e => setEmail(e.target.value)}
+                  />
+              </div>
+              <div class="input-with-label">
+                  <label>Enter your password:</label>
+                  <input 
+                  type="password" 
+                  placeholder="Enter your password"
+                  value={ password }
+                  onChange={ e => setPassword(e.target.value)}
+                  />
+              </div>
+              
+              <div class="btn-link">
+                  <Link to='/signup' className='go-sign-up'>Don't have an account?<br/>Create one here!</Link>
+                  <button type="button" className="signin-btn" id="signin-btn">Signin</button>
+              </div>
+              </div>
+            </div>        
+    )
+}
+
+export default Signin
