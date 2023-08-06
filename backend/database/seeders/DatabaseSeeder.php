c19999881fdb1cc65302cf46333444a531ddb2be
Jaafar Mortada <jaafar1899@outlook.com>
2023-08-06 18:56:49 +0300

    added database seeder

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -2,21 +2,22 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+// use Illuminate\Support\Facades\Hash;
+// use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
-    {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+    {   
+        for ($i = 0; $i < 153; $i++) {
+            DB::table('contacts')->insert([
+                'name' => 'Contact' . $i,
+                'phone_number' => '+'.rand(0, 9) . rand(0, 9). rand(0, 9). rand(0, 9). rand(0, 9). rand(0, 9). rand(0, 9). rand(0, 9) ,
+                'latitude' => rand(-1, 55) ,
+                'longitude' => rand(-1, 55) ,
+            ]);
+    }
     }
 }
